f1c39fb3cca179a26abce8c42b0dcbf01f434851
Pocketloop <87960636+Pocketloop@users.noreply.github.com>
2023-02-23 15:31:42 +0800

    Update utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -13,17 +13,14 @@ export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight,
             //set styling
             ctx.strokeStyle = labelMap[text]['color']
             ctx.lineWidth = 5
-            ctx.fillStyle = 'blue'
+            ctx.fillStyle = 'red'
             ctx.font = '30px Arial'
 
             //Draw
             ctx.beginPath()
             ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
-            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/1.5);
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/1.25, height*imgHeight/1.5);
             ctx.stroke()
-            ctx.strokeRect(x*imgWidth, y*imgHeight, width*imgWidth, height*imgHeight);
-
-            console.log(x*imgWidth, y*imgHeight, width*imgWidth, height*imgHeight)
         }
     }
 }
